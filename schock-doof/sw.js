1b4f01eea9d4ac5f6a1c3c8370c9a603e3b02aef
werbung0816-afk <werbung0816@gmail.com>
2026-06-21 23:08:35 +0200

    Add files via upload

--- schock-doof/sw.js
+++ schock-doof/sw.js
@@ -13,12 +13,12 @@ const CORE_ASSETS = [
   './Jingle1.mp3',
   './Jingle2.mp3',
   './what-a-fuck.mp3',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-192.png',
-  './icons/icon-maskable-512.png',
-  './icons/apple-touch-icon.png',
-  './icons/favicon-32.png'
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-192.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png',
+  './favicon-32.png'
 ];
 
 self.addEventListener('install', (event) => {
