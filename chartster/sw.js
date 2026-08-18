8043f8b14e799ba52cc8247323516e785f99b3d4
werbung0816-afk <werbung0816@gmail.com>
2026-08-18 19:48:03 +0200

    Add files via upload

--- chartster/sw.js
+++ chartster/sw.js
@@ -9,8 +9,8 @@
  *  - Google Fonts → Cache First
  */
 
-const CACHE_NAME = 'chartster-v115';
-const DATA_CACHE = 'chartster-data-v115';
+const CACHE_NAME = 'chartster-v116';
+const DATA_CACHE = 'chartster-data-v116';
 
 // App Shell – alles was für den Grundbetrieb gebraucht wird
 const SHELL_ASSETS = [
